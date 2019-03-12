39d10a1fd615b06f0d964ccb49e1e009cbc01e5c
Arles96 <arles.cerrato@unitec.edu>
2019-03-12 13:06:38 -0600

    make.sh

---
+++ make.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+rm *.class
+rm ada95.java
+rm parser.java
+rm sym.java
+
+jflex ada95.flex
+
+java -cp .:java-cup-11b.jar java_cup.Main < parser.cup
+javac -cp .:java-cup-11b.jar Main.java
+java -cp .:java-cup-11b.jar Main $1

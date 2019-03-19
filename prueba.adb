96ab6529f1abac2d7a8b64e5206f962acc4c090b
Arles96 <arles.cerrato@unitec.edu>
2019-03-19 08:44:44 -0600

    funcion de return sencillo

--- prueba.adb
+++ prueba.adb
@@ -1,5 +1,12 @@
 procedure Hello is
-  hola, hola2: Integer; -- hola
+
+function hola return Integer is
+  hello2 : Integer;
+  begin
+    hello2 := 2;
+  end hola;
+  hello3 : Integer;
+
 begin
   hola := hola2 + 23 / 24;
   put(hola);

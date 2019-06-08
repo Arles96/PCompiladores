d2461704d73feb3946738068b293bb03b52aaaa6
Arles96 <arles.cerrato@unitec.edu>
2019-06-08 16:06:26 -0600

    Se agrego nueva clase para administras los tags de los nodos

---
+++ Nodos/TagAbstract.java
@@ -0,0 +1,93 @@
+package Nodos;
+
+public class TagAbstract {
+
+  // tag de inicio
+  public static final String INICIO = "Nodo de inicio";
+
+  // tag de procedimiento
+  public static final String PROCEDURE = "Procedure";
+
+  // tag de temporales
+  public static final String TEM = "tem";
+
+  // tag de funciones
+  public static final String FUNCTION = "Function";
+
+  // tag de retorno de una funcion
+  public static final String RETURN = "Return";
+
+  // tag de begin (cuerpo de un procedimiento o funcion)
+  public static final String CUERPO = "Cuerpo";
+
+  // tag de id (variables)
+  public static final String ID = "ID";
+
+  // tag de arreglo de declaración
+  public static final String DECLARACION = "Declaracion";
+
+  // tag de tipo de datos
+  public static final String DATATYPE = "DATA TYPE";
+
+  // tag de asignacion
+  public static final String ASSIGN = "ASSIGN";
+
+  // tag de nodo nulo
+  public static final String NULO = "nulo";
+
+  // tag de else de ada
+  public static final String ELSE = "Else";
+
+  // tag de elseif de ada
+  public static final String ELSEIF = "ELSIF";
+
+  // tag de asignacion de valores dentro del cuerpo
+  public static final String ASSINGVALUE = "Asignacion";
+
+  // tag de booleano verdadero
+  public static final String TRUE = "TRUE";
+
+  // tag de booleano falso
+  public static final String FALSE = "FALSE";
+
+  // tag de ejecucion de funcion o proceso
+  public static final String EJECUCION = "Ejecucion";
+
+  // tag de arreglo de argumentos
+  public static final String ARGUMENTOS = "Argumentos";
+
+  // tag de operaciones arigmeticas
+  public static final String OP = "OP";
+
+  // tag de mod para ada
+  public static final String MOD = "MOD";
+
+  // tag de loop de ada
+  public static final String LOOP = "Loop";
+
+  // tag de for de ada
+  public static final String FOR = "FOR";
+
+  // tag de while de ada
+  public static final String WHILE = "WHILE";
+
+  // tag de and de ada
+  public static final String AND = "AND";
+
+  // tag de oprel de ada
+  public static final String OPREL = "OPREL";
+
+  // tag de or de ada
+  public static final String OR = "OR";
+
+  // tag de la funcion GET
+  public static final String GET = "Get";
+
+  // tag de la funcion PUT
+  public static final String PUT = "Put";
+
+  // tag de una cadena dentro de la funcion put
+  public static final String STRING = "String";
+
+
+}
